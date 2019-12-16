c748ff0b704d73588365f94ea030a4398a340bd4
Jakob Kogler <jakob.kogler@gmail.com>
2019-12-16 22:44:59 +0100

    Day 16 (star 2)

--- day16.d
+++ day16.d
@@ -5,28 +5,45 @@ import std.conv;
 import std.string;
 import std.math;
 
-string fft(string input) {
-    auto arr = input.map!`to!int(a-'0')`.array;
-    return
-        arr.length.iota
-            .map!(i =>
-                 zip(arr,
-                     [0, 1, 0, -1]
-                     .map!(a => a.repeat(i + 1))
-                     .joiner
-                     .cycle
-                     .dropOne
-                    )
-                    .map!(x => x[0] * x[1]).sum.abs % 10
-                )
-            .map!(to!string).join;
+int[] fft(int[] arr) {
+    int[] psum = [0] ~ arr.cumulativeFold!"a + b"(0).array;
+    int sum(long l, long r) { // inclusive
+        r = min(psum.length - 2, r);
+        return psum[r+1] - psum[l];
+    }
+
+    int[] output;
+    foreach(i; 1 .. arr.length+1) {
+        long idx = i - 1;
+        int sign = 1;
+        int cur_sum;
+        while (idx < arr.length) {
+            cur_sum += sign * sum(idx, idx + i - 1);
+            idx += 2*i;
+            sign = 0 - sign;
+        }
+        output ~= cur_sum;
+    }
+
+    return output.map!"a.abs % 10".array;
 }
 
 void main() {
-    string number = readln().strip;
+    auto input = readln().strip;
+    auto numbers = input.map!`to!int(a-'0')`.array;
 
+    // star 1
+    auto star1 = numbers.dup;
     foreach(_; 0 .. 100) {
-        number = fft(number);
+        star1 = fft(star1);
     }
-    writeln(number[0..8]);
+    writeln(star1[0..8].map!(to!string).join);
+
+    // star 2
+    auto star2 = numbers.cycle.take(10_000 * numbers.length).array;
+    foreach(_; 0 .. 100) {
+        writeln(_);
+        star2 = fft(star2);
+    }
+    writeln(star2[to!int(input[0..7])..$][0..8].map!(to!string).join);
 }
